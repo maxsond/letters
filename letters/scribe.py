383f73d92bf7ac778890ef530c52e0d49207f8d2
indigochill <indigochill@gmail.com>
2013-05-10 20:18:51 +0000

    Basic letters will now be written.

--- letters/scribe.py
+++ letters/scribe.py
@@ -1,20 +1,35 @@
+#This file is responsible for writing letters
+
 import actors
 import storyteller
 
 class Letter(object):
 
 	def __init__(self,author,target,motive):
-		self.sal = {'tender': 'Dearest ', 'business': '', 'acquaintance': 'Dear'}
+		self.sal = {'tender': 'Dearest ', 'business': '', 'acquaintance': 'Dear '}
 		self.close = {'tender': 'Love,', 'business': 'Regards,', 'acquaintance': 'Sincerely,'}
 		self.author = author
 		self.target = target
 		self.motive = motive
-	
-	def salutation(self):
-		if roster[author]['relationship'][target] > 5:
-			salu = 'tender'
-		return self.sal[salu] + self.target
+
+	def salutation(self, author, target):
+		if actors.roster[author].relationship[target] > 5:
+                    salu = 'tender'
+                else:
+                    salu = 'acquaintance'
+		return '\n' + self.sal[salu] + self.target + ',\n\n'
+
 	def closing(self):
-		return "Love, " + self.author
+            if actors.roster[self.author].relationship[self.target] > 5:
+                mood = 'tender'
+            else:
+                mood = 'business'
+
+            return self.close[mood] + '\n' + self.author + '\n'
+
 	def body(self):
-		return self.motive
+		return self.motive + '\n\n'
+
+        def lprint(self):
+            print self.salutation(self.author, self.target) + self.body() + self.closing()
+            return 0
